2e65960d424df52d2c05269d946b7eaf2830c536
soluyanow <soluyanow@gmail.com>
2019-03-26 17:54:42 +0300

    Авториация и миграци

---
+++ blog/app/Models/Content.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model
+{
+    private function cCreate()
+    {
+
+    }
+
+    private function cDelete()
+    {
+
+    }
+
+    private function cInsert()
+    {
+
+    }
+
+    private function cGet()
+    {
+
+    }
+
+    //////////////////////
+
+    public function createContent()
+    {
+
+
+    }
+
+
+
+}
